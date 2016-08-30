fd24b71efc66e99e8caf59f5be242bbe93b30089
Caroline Joseph <carbroj@gmail.com>
2016-08-29 19:49:28 -0500

    finished index and show page for resumes added a route

--- app/assets/javascripts/resumeCtrl.js
+++ app/assets/javascripts/resumeCtrl.js
@@ -2,7 +2,13 @@
 (function() {
   angular.module("app").controller("resumeCtrl", function($scope, $http) {
     $scope.setup = function() {
+      $http.get().then(function(response) {
+        $scope.resumes = response.data;
 
+      $http.get().then(function(response) {
+        $scope.individualResume = response.data;
+      });
+      });
     };
   });
 })();
