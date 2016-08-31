0851dd8c40dcf112aeadc4540cc7576518de89d8
zukethenuke <zukethenuke@hotmail.com>
2016-08-30 19:57:25 -0500

    Removed Peters x

--- app/assets/javascripts/resumeCtrl.js
+++ app/assets/javascripts/resumeCtrl.js
@@ -2,7 +2,7 @@
 (function() {
   angular.module("app").controller("resumeCtrl", function($scope, $http) {
     $scope.setup = function() {
-      console.log("setup", x);
+      console.log("setup");
       $http.get("https://pacific-plateau-17386.herokuapp.com/api/v1/students").then(function(response) {
         $scope.resumes = response.data;
 
