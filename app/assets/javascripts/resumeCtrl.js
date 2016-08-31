6860b2abdf6ddbe42cdee2950abf26c3edfe36d3
matvacha <matvacha@yahoo.com>
2016-08-30 20:00:56 -0500

    update show page, index page and application html to look nicer

--- app/assets/javascripts/resumeCtrl.js
+++ app/assets/javascripts/resumeCtrl.js
@@ -2,12 +2,10 @@
 (function() {
   angular.module("app").controller("resumeCtrl", function($scope, $http) {
     $scope.setup = function() {
-      console.log("setup", x);
       $http.get("https://pacific-plateau-17386.herokuapp.com/api/v1/students").then(function(response) {
         $scope.resumes = response.data;
 
   
-        console.log($scope.resumes);
 
         // $http.get().then(function(response) {
         // $scope.individualResume = response.data;
